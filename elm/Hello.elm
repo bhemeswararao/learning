122752d1e16e9b321346874d25d07ce0504ccb6a
John Rees <john@bitsushi.com>
2016-10-08 16:15:44 +0100

    comparisons

--- elm/Hello.elm
+++ elm/Hello.elm
@@ -4,3 +4,15 @@ import Html exposing (text)
 
 main =
     text "Hello"
+
+
+-- function(x) { return x + 1 }
+-- \x -> x + 1
+
+-- function(x,y) { return x + y }
+-- \x y -> x + y
+
+--function add1(x) { return x + 1 }
+--add1 : Int -> Int
+--add1 x =
+--  x + 1
